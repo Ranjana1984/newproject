0e45b1f69d557cd45f46fb00dab4d6243fa5bad2
Ranjana1984 <142005449+Ranjana1984@users.noreply.github.com>
2023-12-02 15:02:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,24 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "Test-server" {
   ami           = "ami-0cbd40f694b804622" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "Test-Instance"
   }
+}
+resource "aws_instance" "Prod-server" {
+  ami           = "ami-0cbd40f694b804622" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "Prod-Instance"
+  }
+}
+output "test_server_ip" {
+  value = aws_instance.Test-server.public_ip
+}
+
+output "prod_server_ip" {
+  value = aws_instance.Prod-server.public_ip
 }
